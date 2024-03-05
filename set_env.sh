6e5b3e5b0cb0194adfc2716af70ef623d3024ecc
xuanbinh313 <xuanbinh313@gmail.com>
2024-03-06 00:34:23 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=abcd1234
+export POSTGRES_HOST=postgresdb.ctueml3wv8kp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-nd9990-dev-wc
+export AWS_REGION=us-east-1
+export AWS_PROFILE=udacity
+export JWT_SECRET=hello
+export URL=http://localhost:8100
